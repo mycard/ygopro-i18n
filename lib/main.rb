32dd09dcf6e4e86d79659a0c02b2ffab02db6bcb
zh99998 <zh99998@gmail.com>
2012-05-08 08:36:33 +0800

    temp

--- lib/main.rb
+++ lib/main.rb
@@ -7,32 +7,32 @@ def import(file)
   raise "file not found" unless File.file? file
   case File.extname(file)
   when ".cdb"
-    import_ygopro_db(file)
+    $contents["cards"] = import_ygopro_db(file)
   when ".conf"
-    import_ygopro_strings(file)
+    $contents["strings"] = import_ygopro_strings(file)
   end
   File.open($file,"w"){|file| YAML.dump({$locale => $contents}, file)}
 end
 def import_ygopro_db(file)
-  $contents["cards"] = {}
+  result = {}
   require 'sqlite3'
   db = SQLite3::Database.new( file )
+  db.results_as_hash = true
   db.execute( "select * from texts" ) do |row|
     print "."
-    number = row.shift
-    name = row.shift
-    lore = row.shift
-    strings = row
-    strings.pop while !strings.empty? and (strings.last.nil? or strings.last.empty?)
-    $contents["cards"][number] = {}
-    $contents["cards"][number]["name"] = name
-    $contents["cards"][number]["lore"] = lore
-    $contents["cards"][number]["strings"] = strings unless strings.empty?
+    number = row["id"]
+    result[number] = {}
+    result[number]["name"] = row["name"]
+    result[number]["lore"] = row["desc"]
+    1.upto(16) do |i|
+      result[number]["str#{i}"] = row["str#{i}"] if row["str#{i}"] and !row["str#{i}"].empty?
+    end
   end
+  result
 end
 def import_ygopro_strings(file)
   open(file, "r:bom|utf-8") do |file|
-    $contents["strings"] = {}
+    result = {}
     file.each_line do |line|
       print "."
       next if line[0,1] != "!"
@@ -40,12 +40,13 @@ def import_ygopro_strings(file)
         type = $1
         id = $2
         string = $3
-        $contents["strings"][type] ||= {}
-        $contents["strings"][type][id] = string
+        result[type] ||= {}
+        result[type][id] = string
       else
         raise line
       end
     end
+    return result
   end
 end
 def translate
@@ -56,13 +57,13 @@ def translate_ygopro_db(file)
   require 'sqlite3'
   db = SQLite3::Database.new( file )
   db.execute('begin transaction')
+  old_cards = import_ygopro_db(file)
   $contents["cards"].each do |number, card|
-    card["strings"] = [""] unless card["strings"]
-    card["strings"].each do |str|
-      str.replace str.encode("UTF-16LE")[0,29].encode("UTF-8") if str
-    end
+    card = old_cards[number].merge card
+    card["name"].replace card["name"].encode("UTF-16LE")[0,5].encode("UTF-8")
     stmt = db.prepare( "replace into texts (id, name, desc, #{1.upto(16).collect{|i|"str#{i}"}.join(', ')}) VALUES (#{(['?']*(16+3)).join(', ')}) "  )
-    stmt.execute(number, card["name"], card["lore"], *card["strings"])
+    strings = 1.upto(16).collect { |i| card["str#{i}"] }
+    stmt.execute(number, card["name"], card["lore"], strings)
   end
   db.execute('commit transaction')
 end
